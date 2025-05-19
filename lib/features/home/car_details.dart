c764fef9191e9b8b486f8db0b1d6ed062824ed9a
MustafaAlneami <mero7383@gmail.com>
2025-05-19 17:40:28 +0300

    Start cooking something

---
+++ lib/features/home/car_details.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class CarDetails extends StatelessWidget {
+  const CarDetails({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
