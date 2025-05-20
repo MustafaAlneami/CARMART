e3065bfebff26dea05b947029a680636ffecee3f
MustafaAlneami <mero7383@gmail.com>
2025-05-20 20:36:28 +0300

    Start cooking something

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,11 @@ import 'package:carmart/features/home/home_page.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env"); // Load environment variables
   await Firebase.initializeApp();
   runApp(const MyApp());
 }
